073a03c81a3708ba4c4d012be37ff9b0809c84a4
yichuantey <ycstudio18@gmail.com>
2023-02-22 14:53:57 +0800

    Add link redirect trace file and trying on extract redirection link

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from selenium.webdriver.common.by import By
 import time
 
 
-
 def start_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
-
+    options.add_extension('/path/to/extension.crx')
     driver = webdriver.Chrome(options=options)
     return driver
 
@@ -29,7 +28,7 @@ def enable_vpn(driver):
     driver.find_element(By.CSS_SELECTOR, '.country-option[data-country="United States"]').click()
     driver.find_element(By.CSS_SELECTOR, '#connect-button').click()
 
-def start_recording():
+# def start_recording():
 
 def clear_cache(driver):
     # Step 3: Open Chrome and clear the cache.
@@ -44,17 +43,31 @@ def clear_cache(driver):
 
 def iprice_website(tp_wesbite):
     driver.get(tp_wesbite)
+    tabs = driver.window_handles
     time.sleep(5)
+    print("Number of tabs: ", len(tabs))
+
     driver.find_element(By.CSS_SELECTOR, 'a[data-ga-trigger="ga-conversion"]').click()
     time.sleep(5)
-    tabs = driver.window_handles
     print(driver.current_url)
+
+    # redirect to merchant website
+    tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
     print(driver.current_url)
+    print("Number of tabs: ", len(tabs))
     driver.find_element(By.CSS_SELECTOR, 'div[class="redirect"]').click()
-    time.sleep(5)
-    print(driver.current_url)
-    current_url = driver.current_url
+
+    return redirection_urls
+
+def extract_information(data):
+    # Step 4: Open the website and perform the action.
+    redirection_url = data[0]
+    # find a substring start from ig to &
+    igsource = redirection_url[redirection_url.find("utm_content=----ig"):redirection_url.find("&")]
+    igsource.strip("utm_content=----ig")
+    print(igsource)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -65,10 +78,12 @@ if __name__ == '__main__':
     clear_cache(driver)
 
     example_link = "https://iprice.sg/compare/epson-ecotank-l3250-a4-wi-fi-all-in-one-ink-tank-printer/"
-    iprice_website(example_link)
+    url_record = iprice_website(example_link)
+    extract_information(url_record)
+
     time.sleep(60)
 
-    recorder.close()
+    # recorder.close()
     driver.quit()
     print('Done')
 
