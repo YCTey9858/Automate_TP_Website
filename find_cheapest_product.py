caba6813bf6af2f458b453241596d7993f3077a1
yichuantey <ycstudio18@gmail.com>
2023-02-28 09:42:47 +0800

    Debuging and finding the cheapest categories

---
+++ find_cheapest_product.py
@@ -0,0 +1,43 @@
+from seleniumwire.undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+def start_driver():
+    options = ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--start-maximized")
+    # chrome_options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    driver = Chrome(options=options)
+    return driver
+
+if __name__ == '__main__':
+    driver = start_driver()
+    country = input('Which country you want: '
+                    'Singapore, Malaysia, Indonesia, Thailand, Philippines, Vietnam, Hong Kong: ')
+
+    # iPrice Country URL
+    country_list = {'Singapore': 'https://www.iprice.sg/', 'Malaysia': 'https://www.iprice.my/', 'Indonesia': 'https://www.iprice.co.id/',
+               'Thailand': 'https://www.iprice.co.th/', 'Philippines': 'https://www.iprice.ph/', 'Vietnam': 'https://www.iprice.vn/',
+               'Hong Kong': 'https://www.iprice.hk/'}
+
+    category = input('Which category you want: '
+                     'If you want to search for all categories, please enter "all": ')
+
+    country
+    store = input('Which store you want: ')
+
+    # if category == 'all':
+    #
+    # else:
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
+    full_url = country_list[country] + '/' + category + '/?store=' + store + '&sort=price.net_asc'
+    driver.get(full_url)
+    time.sleep(60)
+
+
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
-import selenium.webdriver as webdriver
+# from seleniumwire import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
+from seleniumwire.undetected_chromedriver import Chrome, ChromeOptions
 import time
+from datetime import datetime
 
 
 def start_driver():
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("detach", True)
-    options.add_extension('/path/to/extension.crx')
-    driver = webdriver.Chrome(options=options)
+    options = ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--start-maximized")
+    # chrome_options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    driver = Chrome(options=options)
     return driver
+
 
 def enable_vpn(driver):
     # Step 1: Open ExpressVPN login page, login and turn on the VPN.
@@ -28,37 +38,65 @@ def enable_vpn(driver):
     driver.find_element(By.CSS_SELECTOR, '.country-option[data-country="United States"]').click()
     driver.find_element(By.CSS_SELECTOR, '#connect-button').click()
 
+
 # def start_recording():
 
 def clear_cache(driver):
     # Step 3: Open Chrome and clear the cache.
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--disable-extensions")
     driver.get('chrome://settings/clearBrowserData')
     time.sleep(3)
     driver.find_element(By.CSS_SELECTOR, 'body').send_keys('\ue007')
     time.sleep(3)
     print("Cleared cache")
 
-def iprice_website(tp_wesbite):
+
+def iprice_website(driver, tp_wesbite):
+    # Step 2: Open the website and perform the action.
     driver.get(tp_wesbite)
-    tabs = driver.window_handles
-    time.sleep(5)
-    print("Number of tabs: ", len(tabs))
 
+    start_catch = datetime.now()
     driver.find_element(By.CSS_SELECTOR, 'a[data-ga-trigger="ga-conversion"]').click()
+    print("Click on the first product")
     time.sleep(5)
-    print(driver.current_url)
 
-    # redirect to merchant website
     tabs = driver.window_handles
     driver.switch_to.window(tabs[1])
-    print(driver.current_url)
-    print("Number of tabs: ", len(tabs))
-    driver.find_element(By.CSS_SELECTOR, 'div[class="redirect"]').click()
+    time.sleep(5)
+    print("Switch to the second tab")
+
+    # If the redirection happened before the robot know
+    try:
+        driver.find_element(By.CSS_SELECTOR, 'div[class="redirect"]').click()
+        print("Click on the redirect button")
+    except:
+        print("Auto redirect")
+
+    end_catch = datetime.now()
+    redirection_urls = extract_url(driver, start_catch, end_catch)
+
+    list = []
+    for request in driver.requests:
+        if request.response and start_catch < request.response.date < end_catch:
+            list.append(
+                {'url': request.url,
+                 'status': request.response.status_code,
+                 'content': request.response.headers['Content-Type']
+                 })
+    print("Done Catching")
 
     return redirection_urls
+
+def click_button(driver, condition, pointer):
+    driver.find_element(condition, pointer).click()
+    time.sleep(5)
+    return driver
+
+def switch_tab(driver, tab_number):
+    tabs = driver.window_handles
+    driver.switch_to.window(tabs[tab_number])
+    time.sleep(5)
+    print("Number of tabs: ", len(tabs))
+    return driver
 
 def extract_information(data):
     # Step 4: Open the website and perform the action.
@@ -69,6 +107,18 @@ def extract_information(data):
     print(igsource)
 
 
+def extract_url(driver, start_time=None, end_time=None):
+    list = []
+    for request in driver.requests:
+        if request.response and start_time < request.response.date < end_time:
+            list.append(
+                {'url': request.url,
+                 'status': request.response.status_code,
+                 'content': request.response.headers['Content-Type']
+                 })
+    return list
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     load_dotenv()
@@ -77,13 +127,13 @@ if __name__ == '__main__':
 
     clear_cache(driver)
 
-    example_link = "https://iprice.sg/compare/epson-ecotank-l3250-a4-wi-fi-all-in-one-ink-tank-printer/"
-    url_record = iprice_website(example_link)
-    extract_information(url_record)
+    example_link = "https://iprice.co.id/perhiasan/?store=blibli&sort=price.net_asc"
+    # example_link = "https://iprice.sg/compare/xiaomi-11t/#amp-x-autoplay=1"
+    url_record = iprice_website(driver, example_link)
+    # extract_information(url_record)
 
     time.sleep(60)
 
     # recorder.close()
     driver.quit()
     print('Done')
-
