cee6dbb9d07d3bc303a7068b5751708ae35a3ec5
yichuantey <ycstudio18@gmail.com>
2023-03-20 16:13:34 +0800

    Login and generate script

--- check_event_tracer.py
+++ check_event_tracer.py
@@ -18,4 +18,36 @@ def start_driver():
     return driver
 
 if __name__ == '__main__':
-    pass
+    driver = start_driver()
+
+    driver.get('https://partner.ipricegroup.com/admin/stats/event_tracer')
+
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+    driver.find_element(By.XPATH, '//*[@id="UserEmail"]').send_keys(username)
+    driver.find_element(By.XPATH, '//*[@id="UserPassword"]').send_keys(password)
+
+    driver.find_element(By.XPATH, '//*[@id="loginButton"]').click()
+
+    time.sleep(5)
+
+    driver.get('https://partner.ipricegroup.com/admin/stats/event_tracer')
+
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[3]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[4]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[5]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[6]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[7]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[2]/div[2]/div/ul/li[8]/span/input').click()
+
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[4]/div[2]/div/ul/li[3]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[4]/div[2]/div/ul/li[4]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[4]/div[2]/div/ul/li[5]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[4]/div[2]/div/ul/li[6]/span/input').click()
+    driver.find_element(By.XPATH, '//*[@id="qed"]/div/div/div[1]/div/div/div[1]/form/div[2]/div[2]/div/ul/div[4]/div[2]/div/ul/li[7]/span/input').click()
+
+    driver.find_element(By.XPATH, '//*[@id="run-eventLog"]').click()
+
+    driver.find_element(By.XPATH, '//*[@id="export-eventLog"]').click()
+
+    print(driver.page_source)
